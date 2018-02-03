96f530c8618473e25ee4cdbdeeb013a01ad133fc
Haroldas Uznys <you@example.com>
2018-02-03 15:56:50 +0200

    first commit

---
+++ app/src/main/java/harusee/to_do/to_doapp/Model/ItemVO.java
@@ -0,0 +1,11 @@
+package harusee.to_do.to_doapp.Model;
+
+/**
+ * Created by Haroldas on 03/02/2018.
+ */
+
+public class ItemVO {
+    public int id;
+    public int done;
+    public String title;
+}
